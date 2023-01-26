b8511c6dfb31774f9b9a719fe975d07dd10fd206
jennifergri <jennifergr@hotmail.no>
2023-01-26 02:25:20 +0100

    Forblir farge ved klikk
    Fikk til noe
    
    kommenterte også ut html infoen for å få blank forside

--- arbeidskrav_2/ressurser.js
+++ arbeidskrav_2/ressurser.js
@@ -95,37 +95,29 @@ const resources = [
     },
 ]
 
-
 //Inspirasjon av Legodudes kode fra forelesning 20 jan - oppgave 4.
 //Inspirasjon min eksamensinnlevering i innprog - minstekrav 2
+//Hjelp av studass
 
-const button = document.getElementById("button")
 
-function colorButton(){
-    if (button.classList.contains("active")){
-        button.classList.add("active")
-        document.getElementById("button").style.backgroundColor = '#fff';
-    } else {
-        button.classList.remove("active")
-    }
-}
+//Ville ha main på forsiden tom når siden oppdateres!
 
-let navigasjon = ""
+//Navigasjon:
+let menu = ""
 
 function addNav(){
     resources.map((info, index) => {
-        navigasjon += `<button id=${index} onClick="addSources(${index})">${info.category}</button>`
+        menu += `<button class="btn" id=${index} onClick="addInfo(${index})">${info.category}</button>`
     })
 }
 addNav()
-document.getElementById("nav").innerHTML = navigasjon
+document.getElementById("nav").innerHTML = menu
 
-
-let categoryInfo = ""
-let links = ""
-function addSources(index){
-    links = ""
+//Informasjon i main:
+function addInfo(index){
     categoryInfo = ""
+    links = ""
+    
         categoryInfo += `
         <h2>${resources[index].category}</h2>
         <p>${resources[index].text}</p>
@@ -133,17 +125,21 @@ function addSources(index){
         
         resources[index].sources.map(element => {
             links += `<li><a href="${element.url}">${element.title}</a></li>`
-            console.log(element)
         })
-    
+        
     document.getElementById("informasjon").innerHTML = categoryInfo
     document.getElementById("linker").innerHTML = links
 
-    console.log(links)
 }
 
+//Button blir turkis ved klikk
+    //Funker kun på HTML knapp og jeg fikk ikke til at knappen slutta å bli turkis igjen når jeg trykker på en ny knapp - men beholdt det jeg fikk til:
 
+    const button = document.querySelector(".btn");
 
-
-    
-
+    button.addEventListener('click', function addNav() {
+        button.style.backgroundColor = 'var(--aqua)';
+        button.style.border = 'var(--aqua)';
+        button.style.color = 'var(--white)';
+    });
+/*Kilde: <a href="https://bobbyhadz.com/blog/javascript-change-button-color-onclick"</a>*/
